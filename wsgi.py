7ec59235d958472ad958640bac5059e624ad02e3
John Graham <john.graham@virtua-enterprise.co.uk>
2017-09-24 14:34:03 +0100

    Updated

--- wsgi.py
+++ wsgi.py
@@ -3,8 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    message = "Hello World, I'm running on Openshift, now with an updated message"
-    return message
+    return "Hello World, I'm running on Openshift, now with an updated message"
 
 if __name__ == "__main__":
-    application.run()
+    application.run()s
